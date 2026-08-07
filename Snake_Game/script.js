47179f3271678c787a1fa8e56e426c448d0ebfad
Kianosh Kia <Kianoshkia4@gmail.com>
2026-08-07 15:11:40 +0000

    the snake was created, snake can be controlled with arrow keys and an alert will appear if the snake hit his head to the walls

--- Snake_Game/script.js
+++ Snake_Game/script.js
@@ -5,8 +5,12 @@ const blockwidth = 100;
 
 const cols = Math.floor(board.clientWidth / blockwidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
+let intervalId = null;
 
 const blocks = [];
+const snake = [{x: 6, y: 5}, {x: 6, y: 6}];
+
+let direction = 'down';
 
 
 
@@ -20,3 +24,69 @@ for( let row = 0; row < rows; row++){
 
     }
 }
+
+
+function render(){
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.add('fill');
+        
+
+    });
+}
+
+intervalId = setInterval(() => {
+
+    let head = null;
+
+    if(direction === "right"){
+        head = {x: snake[0].x, y: snake[0].y + 1}
+
+    }
+    if(direction === "left"){
+        head = {x: snake[0].x, y: snake[0].y - 1}
+
+    }
+    if(direction === "up"){
+        head = {x: snake[0].x - 1, y: snake[0].y}
+
+    }
+    if(direction === "down"){
+        head = {x: snake[0].x + 1, y: snake[0].y}
+
+    }
+
+    if(head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols){
+        alert("Game Over");
+        clearInterval(intervalId);
+        
+    }
+
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove('fill');
+
+
+    });
+
+    // snake.unshift(head);
+    // snake.pop();
+
+    render();
+}, 300);
+
+
+
+addEventListener("keydown", (event) => {
+    if(event.key == "ArrowUp"){
+        direction = "up"
+    }
+    if(event.key == "ArrowDown"){
+        direction = "down"
+    }
+    if(event.key == "ArrowRight"){
+        direction = "right"
+    }
+    if(event.key == "ArrowLeft"){
+        direction = "left"
+    }
+
+});
