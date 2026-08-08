c6a8142d45936ffaed1ad20ee1dc56c48d6e2853
Kianosh Kia <Kianoshkia4@gmail.com>
2026-08-08 10:37:55 +0000

    the core logic of game created, score and highScore was created and score was assigned to highScore , highScore will be saved in Localstorage

--- Snake_Game/script.js
+++ Snake_Game/script.js
@@ -1,18 +1,37 @@
 const board = document.querySelector('.board');
+const startButton = document.querySelector('.btn-start');
+const modal = document.querySelector('.modal');
+const startGameModal = document.querySelector('.start-game');
+const gameOverModal = document.querySelector('.game-over');
+const restartButton = document.querySelector('.btn-restart');
+
+const highScoreElement = document.querySelector('#high-score');
+const scoreElement  = document.querySelector('#score');
+const timeElement = document.querySelector('#time');
+
+
+
 const blockHeight = 100;
 const blockwidth = 100;
 
+ let highScore = localStorage.getItem("highScore") || 0;
+ let score = 0;
+ let time = `00:00`
+
+ highScoreElement.innerText = highScore;
 
 const cols = Math.floor(board.clientWidth / blockwidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
+
 let intervalId = null;
+let timerIntervalId = null;
+
 let food = {x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols)};
 
 const blocks = [];
-const snake = [{x: 6, y: 5}, {x: 6, y: 6}];
+let snake = [{x: 6, y: 5}, {x: 6, y: 6}];
 
 let direction = 'down';
-
 
 
 for( let row = 0; row < rows; row++){
@@ -20,7 +39,7 @@ for( let row = 0; row < rows; row++){
         const block = document.createElement('div');
         block.classList.add("block");
         board.appendChild(block);
-        block.innerText = `${row}-${col}`
+        //block.innerText = `${row}-${col}`
         blocks[`${row}-${col}`] = block;
 
     }
@@ -48,17 +67,39 @@ function render(){
         head = {x: snake[0].x + 1, y: snake[0].y}
 
     }
-
+         // wall collision logic
     if(head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols){
-        alert("Game Over");
         clearInterval(intervalId);
 
-    }
 
+        modal.style.display = "flex";
+        startGameModal.style.display = "none";
+        gameOverModal.style.display = "flex";
+
+        return;
+
+    }
+    
+
+
+
+        // food consumtion logic
     if(head.x == food.x && head.y == food.y){
+
         blocks[`${food.x}-${food.y}`].classList.remove("food");
+
         food = {x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols)};
+
         blocks[`${food.x}-${food.y}`].classList.add("food");
+        snake.unshift(head);
+
+        score += 10;
+        scoreElement.innerText = score;
+
+        if(score > highScore){
+            highScore = score;
+            localStorage.setItem("highScore", highScore.toString());
+        }
 
     }
 
@@ -69,8 +110,8 @@ function render(){
 
     });
 
-    // snake.unshift(head);
-    // snake.pop();
+    snake.unshift(head);
+    snake.pop();
 
 
     snake.forEach(segment => {
@@ -84,9 +125,61 @@ intervalId = setInterval(() => {
 
 
     render();
-}, 300);
+}, 100);
+
+startButton.addEventListener('click', (event) => {
+    modal.style.display = "none";
+    intervalId = setInterval(() => {render() },200);
+
+    timerIntervalId = setInterval(() => {
+        const  [min, sec] = time.split(":").map(Number)
+
+        if(sec == 59) {
+            min += 1;
+            sec = 0;
+
+        }
+        else {
+            sec += 1;
+
+        }
+
+        time = `${min}-${sec}`;
+        timeElement.innerText = time;
+
+    }, 1000);
+        
+
+   
+
+});
+
+restartButton.addEventListener('click', restartGame);
+
+function restartGame() {
+    blocks[`${food.x}-${food.y}`].classList.remove("food");
+
+        snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove('fill');
+        
+
+    });
+
+    score = 0;
+    time = `00:00`;
 
 
+    scoreElement.innerText = score;
+    timeElement.innerText = time;
+    highScoreElement.innerText = highScore;
+
+    modal.style.display = "none";
+    snake = [{x: 1, y: 3}];
+    food = {x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols)};
+    intervalId = setInterval(() => {render()}, 300);
+        
+
+}
 
 addEventListener("keydown", (event) => {
     if(event.key == "ArrowUp"){
